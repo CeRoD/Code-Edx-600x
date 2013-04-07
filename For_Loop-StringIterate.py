3d79c0ad32a59242ea8500b6a008d79b9eb97e33
Cesar Rodriguez <cesar.rd@gmail.com>
2013-04-06 23:10:36 -0500

    Added two new For loops: Cube and String Iterate.

---
+++ For_Loop-StringIterate.py
@@ -0,0 +1,5 @@
+total = 0
+for c in '1.23,4.56,7.89':
+    print c
+    total = total + float(c)
+print total
---
+++ For_Loop_CubeRoot.py
@@ -0,0 +1,15 @@
+# Find the cube root of a perfect cube
+
+x = int(raw_input('Enter an integer: '))
+
+for ans in xrange (0, abs(x)+1):
+            ## xrange is a lot more effient that range.
+    print ans # Added for munra to check the values that ans take.
+    if ans**3 == abs(x):
+        break
+if ans**3 != abs(x):
+    print x, 'is not a perfect cube.'
+else:
+    if x < 0:
+        ans = -ans
+    print 'Cube root of ' + str(x) + ' is ' + str(ans)
